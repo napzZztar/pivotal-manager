7a988f2a68e18e1d46b6282b357261a94eff1809
Ashik Ahmed <ashik992@gmail.com>
2018-11-17 22:33:16 +0600

    Implement feature tabs and add meetign note edit page

---
+++ src/app/meeting-note-edit/meeting-note-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meeting-note-edit',
+  templateUrl: './meeting-note-edit.component.html',
+  styleUrls: ['./meeting-note-edit.component.scss']
+})
+export class MeetingNoteEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
